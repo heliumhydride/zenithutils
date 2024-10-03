31a1832e13fcf7124b6c7b273220f68500c4da3f
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-10-03 15:20:57 +0200

    add compiling flags to get more info on what could go wrong + other stuff

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 CC = cc
 LD = $(CC)
 
-CFLAGS = -O2 -std=c99
+CFLAGS = -O2 -std=c99 -Wall -Wextra -Wpedantic
 LDFLAGS = $(CFLAGS)
 WIN32 = 0
 # WINNT_ALIAS_CFLAGS = -Os -nostdlib -lkernel32 -lntdll -lmingw32
