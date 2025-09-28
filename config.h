a935e2d7cd1facccfe006b1bf808b2eb56944aa4
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2025-09-28 13:55:14 +0200

    define some variables as preprocessor macros to avoid defined but not used warnings

--- config.h
+++ config.h
@@ -21,14 +21,14 @@
   Base32 and Base64 alphabets
 */
 
-static const char* base32ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-static const char* base64ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+#define base32ab "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+#define base64ab "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 /*
   exedump output colors (uses ANSI escape color codes)
 */
 
-static char* exedump_col1 = "\033[1;34m";
-static char* exedump_col2 = "\033[1;31m";
+#define exedump_col1 "\033[1;34m"
+#define exedump_col2 "\033[1;31m"
 
 #endif
--- include/colors.h
+++ include/colors.h
@@ -1,10 +1,11 @@
 // ZU = ZenithUtils
 #ifndef ZU_COLORS_H
 #define ZU_COLORS_H
-static char* ANSI_RED = "\033[1;31m";
-static char* ANSI_YELLOW = "\033[1;32m";
-static char* ANSI_GREEN = "\033[1;33m";
-static char* ANSI_BLUE = "\033[1;34m";
-static char* ANSI_NORM = "\033[0m";
+
+#define ANSI_RED    "\033[1;31m"
+#define ANSI_YELLOW "\033[1;32m"
+#define ANSI_GREEN  "\033[1;33m"
+#define ANSI_BLUE   "\033[1;34m"
+#define ANSI_NORM   "\033[0m"
 
 #endif // ZU_COLORS_H
