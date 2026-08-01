3e9f1d65d72e8225f96fff63f3fc488ab6c62d44
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2026-08-01 15:10:26 +0200

    coreutils/seq: correct formating and allow -w

--- coreutils/seq.c
+++ coreutils/seq.c
@@ -8,6 +8,33 @@
 #include "../include/prettyprint.h"
 #include "../config.h"
 
+#ifndef BUFSIZE
+#define BUFSIZE 256
+#endif
+
+int digits_left(const char* s) {
+  int i = 0;
+  while(s[i] != '.' && s[i] != '\0')
+    i++;
+  return i;
+}
+
+int digits_right(const char* s) {
+  size_t size = strlen(s) - 1;
+  int has_decimal = 0;
+  int i = size;
+  while(i >= 0 && s[i] != '.') {
+    i--;
+  }
+  if(s[i] == '.')
+    has_decimal = 1;
+  return has_decimal ? size-i : 0;
+}
+
+int max(float first, float increment, float last) {
+  return (increment > 0) ? last : first;
+}
+
 void print_usage(char* program) {
   fprintf(stderr, "usage: %s [-f fmt] [-s sep] last\n", program);
   fprintf(stderr, "       %s [-f fmt] [-s sep] first last\n", program);
@@ -76,11 +103,14 @@ int is_sane_fmt(const char* fmt) {
 
 int main(int argc, char* argv[]) {
   char* program = argv[0];
-
-  char* fmt = "%g";
+  char buf[BUFSIZE];
+  char* fmt = buf;
   char* sep = "\n";
   int wflag = 0;
+  int fflag = 0;
 
+  char* first_s = "1";
+  char* last_s = "1";
   float first = 1.0f;
   float increment = 1.0f;
   float last = 1.0f;
@@ -101,13 +131,22 @@ int main(int argc, char* argv[]) {
       break;
     switch(opt) {
       case 'f': // set format for printf, default is "%g"
+        fflag = 1;
+        if(wflag) {
+          print_error("%s: -f may not be specified with -w", program);
+          return 1;
+        }
         fmt = optarg;
         break;
       case 's': // separator, default is newline
         sep = optarg;
         break;
-      case 'w': // TODO
+      case 'w': // zero padding
         wflag = 1;
+        if(fflag) {
+          print_error("%s: -f may not be specified with -w", program);
+          return 1;
+        }
         break;
       case ':':
         print_error("%s: option '-%c' needs an argument", program, optopt);
@@ -133,29 +172,33 @@ int main(int argc, char* argv[]) {
       return 1;
       break;
     case 1: // just last
-      last = strtof(argv[1], &endptr);
-      if(endptr == argv[1]) {
-        print_error("%s: %s: not a number", program, argv[1]);
+      last_s = argv[1];
+      last = strtof(last_s, &endptr);
+      if(endptr == last_s) {
+        print_error("%s: %s: not a number", program, last_s);
         return 1;
       }
       break;
     case 2: // first last
-      first = strtof(argv[1], &endptr);
-      if(endptr == argv[1]) {
-        print_error("%s: %s: not a number", program, argv[1]);
+      first_s = argv[1];
+      first = strtof(first_s, &endptr);
+      if(endptr == first_s) {
+        print_error("%s: %s: not a number", program, first_s);
         return 1;
       }
 
-      last = strtof(argv[2], &endptr);
-      if(endptr == argv[2]) {
-        print_error("%s: %s: not a number", program, argv[2]);
+      last_s = argv[2];
+      last = strtof(last_s, &endptr);
+      if(endptr == last_s) {
+        print_error("%s: %s: not a number", program, last_s);
         return 1;
       }
       break;
     case 3: // first increment last
-      first = strtof(argv[1], &endptr);
-      if(endptr == argv[1]) {
-        print_error("%s: %s: not a number", program, argv[1]);
+      first_s = argv[1];
+      first = strtof(first_s, &endptr);
+      if(endptr == first_s) {
+        print_error("%s: %s: not a number", program, first_s);
         return 1;
       }
 
@@ -165,9 +208,10 @@ int main(int argc, char* argv[]) {
         return 1;
       }
 
-      last = strtof(argv[3], &endptr);
-      if(endptr == argv[3]) {
-        print_error("%s: %s: not a number", program, argv[3]);
+      last_s = argv[3];
+      last = strtof(last_s, &endptr);
+      if(endptr == last_s) {
+        print_error("%s: %s: not a number", program, last_s);
         return 1;
       }
       break;
@@ -176,11 +220,26 @@ int main(int argc, char* argv[]) {
       return 1;
   }
 
-  // sanity checks
   if(increment == 0) {
     print_error("%s: refusing zero increment", program);
     return 1;
   }
+
+  char* s;
+  if(max(first, increment, last) == first) {
+    s = first_s;
+  } else {
+    s = last_s;
+  }
+
+  if(!fflag) {
+    int left = digits_left(s) + digits_right(s);
+    if(wflag)
+      snprintf(buf, BUFSIZE, "%%0%d.%df", left, digits_right(s));
+    else
+      snprintf(buf, BUFSIZE, "%%%d.%df", left, digits_right(s));
+  }
+
   if(!is_sane_fmt(fmt)) {
     print_error("%s: insane fmt '%s', incorrect for a single floating point numbers", program, fmt);
     return 1;
