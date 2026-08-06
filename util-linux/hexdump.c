0855d3b6182c17f7198fe8afe755b9e569264478
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2026-08-06 18:02:24 +0200

    fix hexdump not printing last hex line

--- util-linux/hexdump.c
+++ util-linux/hexdump.c
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <ctype.h>
 #include "../include/prettyprint.h"
 
-// TODO last ascii representation not being printed at the end
 // TODO options
 
 void print_ascii_representation(const uint8_t* buf) {
@@ -44,14 +44,24 @@ int main(int argc, char* argv[]) {
     if((addr % 0x10) == 0x08) putchar(' ');
 
     printf("%02x ", (uint8_t)c);
-    buf[addr % 0x10] = (uint8_t)c;
+    if(c == '\0')
+      buf[addr % 0x10] = '.'; // secret magic trick, avoids cutting the buffer because we check using '\0'. we can do that because NUL would display as '.' anyway
+    else
+      buf[addr % 0x10] = (uint8_t)c;
 
     if((++addr % 0x10) == 0) {
       print_ascii_representation(buf);
       putchar('\n');
+      memset(buf, 0, 0x10);
     }
   }
-  if((addr % 0x10) > 0) putchar('\n');
+  if((addr % 0x10) > 0) {
+    // TODO incorrect amount of spaces sometimes
+    int spaces = 16*3 - (int)(strlen((char*)buf)*3);
+    printf("%*s", spaces, " ");
+    print_ascii_representation(buf);
+    putchar('\n');
+  }
   printf("%08zx\n", addr);
 
   fclose(fp);
