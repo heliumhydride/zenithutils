a763e6e227ca88ae6c5d7838725731e949bbce95
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2026-08-11 13:30:46 +0200

    coreutils/tail rework

--- coreutils/tail.c
+++ coreutils/tail.c
@@ -8,29 +8,46 @@
 #include <unistd.h>
 //#include <string.h>
 
+#define STB_DS_IMPLEMENTATION
+#include "../include/stb_ds.h"
+
 #include "../include/prettyprint.h"
 #include "../include/util.h"
-#include "../config.h"
+// #include "../config.h"
 
 void print_usage(char* argv0) {
-  fprintf(stderr, "usage: %s [-f] [-n int] [-v|-q]\n", argv0);
+  fprintf(stderr, "usage: %s [-f] [-s float] [-n int] [-v|-q]\n", argv0);
 }
 
-int main(int argc, char* argv[]) {
-  int opt;
-  size_t n_lines = 10; // default: -n10
-  int verbosity = 1; // quiet:0 normal:1 full:2
-  char* program = argv[0];
-  int fflag = 0; // follow file output
+// TODO tail -f, make use of -s
 
-  // If -q is used in conjuction with -v, -v takes the upper hand. (that's what you get for using both options!)
-  while((opt = getopt(argc, argv, ":n:fqv")) != -1) {
+int main(int argc, char* argv[]) {
+  char* program = argv[0];
+  char mode = 'n';
+  long num = 10; // default: -n10
+  int verbosity = 1; // quiet:0 normal:1 full:2
+  int fflag = 0; // follow file output
+  float sleep_interval = 1.0f; // in seconds
+
+  char* endptr;
+  int opt;
+  while((opt = getopt(argc, argv, ":n:s:fqv")) != -1) {
     switch(opt) {
+      case 'c': // fallthrough
       case 'n':
-        if(str_is_nan(optarg)) {
-          print_error("%s: invalid integer '%s'", program, optarg);
+        mode = (char)opt;
+        num = strtol(optarg, &endptr, 10);
+        if(endptr == optarg) {
+          print_error("%s: %s: not an integer", program, optarg);
+          return 1;
         }
-        n_lines = (size_t)atol(optarg);
+        break;
+      case 's':
+        sleep_interval = strtof(optarg, &endptr);
+        if(endptr == optarg) {
+          print_error("%s: %s: not a floating point number", program, optarg);
+          return 1;
+        }
         break;
       case 'f':
         fflag = 1;
@@ -53,47 +70,61 @@ int main(int argc, char* argv[]) {
         break;
     }
   }
-  optind--;
 
-  argv += optind;
-  if(!argv[0]) {
-    print_usage(program);
+  argc -= optind;
+  argv += optind - 1;
+
+  if(argc == 0) {
+    argv--;
+    argv[1] = "-";
   }
 
-  ssize_t filesize;
-  FILE* fileptr;
-  char* line;
-  size_t line_counter;
-
+  FILE* fp;
   while(*++argv) {
-    if((argc == optind) || strcmp(*argv, "-")) { // Read from stdin if no argument other than opts are provided / file is -
-      filesize = STDIN_MAX;
-      fileptr = stdin;
-      if(verbosity >= 2)
-        printf("==> standard input <==\n");
-    } else { // We read from a file
-      fileptr = fopen(*argv, "r");
-      if(NULL == fileptr) {
-        print_error("%s: could not open file \"%s\"", program, *argv);
-        return 1;
-      }
-      filesize = get_filesize(fileptr);
+    fp = stdin;
+    if(strcmp(*argv, "-"))
+      fp = fopen(*argv, "r");
+
+    if(fp == NULL) {
+      fprintf(stderr, "%s: %s: could not open file", program, *argv);
+      return 1;
     }
 
-    line = malloc(filesize);
-    line_counter = 1;
-    if((verbosity >= 2) && (fileptr != stdin))
-      printf("==> %s <==\n", *argv);
-    while(((line = fgets(line, filesize, fileptr)) != NULL) && (line_counter > n_lines)) {
-      line[strlen(line)-1] = '\0'; // strip '\n' off the string
-      for(size_t i = 0; i <= strlen(line); i++) {
-        printf("%c", line[i]);
+    if(verbosity == 2)
+      printf("==> %s <==\n", (fp == stdin) ? "standard input" : *argv);
+
+    if(mode == 'c') {
+      long i = 0;
+      int c;
+      while((c = fgetc(fp)) != EOF) {
+        if(i > num) putchar(c);
+        i++;
       }
-      printf("\n");
-      line_counter++;
+    } else {
+      size_t cap = 0;
+      char* line = NULL;
+      char** line_list = NULL;
+      while(getline(&line, &cap, fp) != -1) {
+        char* line2 = malloc(strlen(line) + 1);
+        if(line2 == NULL) {
+          print_error("%s: malloc() failed", program);
+          return 2;
+        }
+        strcpy(line2, line);
+        stbds_arrput(line_list, line2);
+      }
+      size_t n_lines = arrlenu(line_list);
+      for(size_t i = n_lines - num; i < n_lines; i++)
+        printf("%s", line_list[i]);
+      stbds_arrfree(line_list);
     }
-    fclose(fileptr);
-    free(line);
+
+    if(fp != stdin)
+      fclose(fp);
+
+    if(argc >= 2)
+      putchar('\n');
   }
+
   return 0;
 }
